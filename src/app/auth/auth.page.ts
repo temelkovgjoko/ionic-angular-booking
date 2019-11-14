49c34059a2b63dad72b21d08ea285a4cc8ab16a8
Gjoko <temelkovgjoko@gmail.com>
2019-11-15 00:22:49 +0100

    authentication form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,7 +11,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
-
+  isLogin = true;
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() { }
@@ -27,5 +28,22 @@ export class AuthPage implements OnInit {
         }, 1500)
       });
     this.authService.login();
+  }
+
+  onSwitchAuthModel() {
+    this.isLogin = !this.isLogin
+  }
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password)
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+
+
   }
 }
